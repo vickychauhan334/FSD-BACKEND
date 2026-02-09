13de4523dfb9ad554cc1e5f6d0f3884277159901
vickychauhan334 <vickysinghchauhan789@gmail.com>
2026-02-09 12:01:01 +0530

    day1 part 2

--- day1/promises/index.js
+++ day1/promises/index.js
@@ -1,9 +1,12 @@
-const getData = async () =>
+const getData = async () =>{
+  try
 {
     const response = await fetch('https://fakestoreapi.com/products');
     const res =await response.json()
     console.log(res);
 
+} catch (error){
+  console.log("unable to fetch data")
 }
 getData();
 
